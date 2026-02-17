fe5f00c0ef2436cdd590f3725e4c252e446ff0a1
Priyal232 <priyalcjm@gmail.com>
2026-02-17 13:59:02 +0530

    uc2 done

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.printf("%s\n", "*****  *****  *****  *****");
+        System.out.printf("%s\n", "*   *  *   *  *   *  *    ");
+        System.out.printf("%s\n", "*   *  *   *  *   *  *    ");
+        System.out.printf("%s\n", "*   *  *   *  *   *  *    ");
+        System.out.printf("%s\n", "*   *  *   *  *****  ***** ");
+        System.out.printf("%s\n", "*   *  *   *  *          * ");
+        System.out.printf("%s\n", "*   *  *   *  *          * ");
+        System.out.printf("%s\n", "*   *  *   *  *          * ");
+        System.out.printf("%s\n", "*****  *****  *      *****");
+    }
+}
