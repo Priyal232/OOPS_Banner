25d170bd3161f00816157c25322c5352900c6811
07pz1491 <pz1491@srmist.edu.in>
2026-03-10 23:51:56 +0530

    uc4

--- OOPS.java
+++ OOPS.java
@@ -1,13 +1,33 @@
-class OOPS{
+public class OOPS{
     public static void main(String[] args) {
 
-        System.out.println(String.join("", "      ***      ***      *****       ***** "));
-        System.out.println(String.join("", "     ** **    ** **    **   **    **      "));
-        System.out.println(String.join("", "    **   **  **   **   **   **   **       "));
-        System.out.println(String.join("", "    **   **  **   **   *****      ***     "));
-        System.out.println(String.join("", "    **   **  **   **   **            **   "));
-        System.out.println(String.join("", "     ** **    ** **    **             **  "));
-        System.out.println(String.join("", "      ***      ***     **        *****    "));
+        String[] O = {
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
 
+        String[] P = {
+                " ******  ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       "
+        };
+
+        String[] S = {
+                "  *****  ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                "  *****  "
+        };
+
+        // Print OOPS banner
+        for (int i = 0; i < 5; i++) {
+            System.out.println(O[i] + "   " + O[i] + "   " + P[i] + "   " + S[i]);
+        }
     }
 }
