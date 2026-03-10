4ff83099147e769e03473ffb1dda585c5bd5e8f9
07pz1491 <pz1491@srmist.edu.in>
2026-03-10 14:14:39 +0530

    done

--- OOPS.java
+++ OOPS.java
@@ -1,13 +1,13 @@
 class OOPS{
     public static void main(String[] args) {
 
-        System.out.println("      ***      ***      *****       *****      ");
-        System.out.println("     ** **    ** **    **   **    **           ");
-        System.out.println("    **   **  **   **   **   **   **            ");
-        System.out.println("    **   **  **   **   *****      ***          ");
-        System.out.println("    **   **  **   **   **            **        ");
-        System.out.println("     ** **    ** **    **             **       ");
-        System.out.println("      ***      ***     **        *****         ");
+        System.out.println(String.join("", "      ***      ***      *****       ***** "));
+        System.out.println(String.join("", "     ** **    ** **    **   **    **      "));
+        System.out.println(String.join("", "    **   **  **   **   **   **   **       "));
+        System.out.println(String.join("", "    **   **  **   **   *****      ***     "));
+        System.out.println(String.join("", "    **   **  **   **   **            **   "));
+        System.out.println(String.join("", "     ** **    ** **    **             **  "));
+        System.out.println(String.join("", "      ***      ***     **        *****    "));
 
     }
 }
